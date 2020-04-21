746e89f3a3c57c7d1c83b8045fe86baac61d552c
jhonny <jhonny.sicol@gmail.com>
2020-04-21 10:51:05 -0300

    updated package to include types only on dev

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,23 +1,23 @@
-import { composeWithDevTools } from "redux-devtools-extension";
-import { Store, createStore, applyMiddleware } from "redux";
+import { Store, createStore, applyMiddleware, compose } from "redux";
 import { ApplicationState, reducers, sagas } from ".";
 import createSagaMiddleware from "redux-saga";
-import reactotron from "../configs/reactotron";
 
 function configureStore(): Store<ApplicationState> {
-  const composeEnhancers = composeWithDevTools({});
   const sagaMiddleware = createSagaMiddleware();
 
   const store = createStore<ApplicationState, any, any, any>(
     reducers,
-    composeEnhancers(applyMiddleware(sagaMiddleware))
- );
- 
+    compose(applyMiddleware(sagaMiddleware))
+  );
+
   sagaMiddleware.run(sagas);
   return store;
 }
 
 function configureStoreDev(): Store<ApplicationState> {
+  const { composeWithDevTools } = require("redux-devtools-extension");
+  const reactotron = require("../configs/reactotron");
+
   const sagaMonitor = reactotron.createSagaMonitor!();
   const actionsMonitor = reactotron.createEnhancer!();
   const composeEnhancers = composeWithDevTools({});
@@ -29,12 +29,13 @@ function configureStoreDev(): Store<ApplicationState> {
   const store = createStore<ApplicationState, any, any, any>(
     reducers,
     composeEnhancers(applyMiddleware(sagaMiddleware), actionsMonitor)
- );
+  );
 
   sagaMiddleware.run(sagas);
 
   return store;
 }
 
-
-export default process.env.NODE_ENV === 'development' ? configureStoreDev : configureStore
+export default process.env.NODE_ENV === "development"
+  ? configureStoreDev
+  : configureStore;
